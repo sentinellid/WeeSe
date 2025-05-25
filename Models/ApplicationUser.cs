dd0ceb937ca1dd9ebd39e57f88fa77f3fecddf84
Daniele Sentinelli <sentinellid@gmail.com>
2025-05-25 09:52:34 +0200

    login più carina

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,9 +5,9 @@ namespace WeeSe.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Role { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Role { get; set; } = String.Empty;
         public DateTime CreatedAt { get; set; }
         public bool IsActive { get; set; }
     }
